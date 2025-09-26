abeb9af073cc3c64135412d713c7202413b0c678
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2025-09-26 12:26:18 +0100

    add books data

---
+++ data.py
@@ -0,0 +1,7 @@
+livres = [
+    {"titre": "1984", "auteur": "George Orwell", "année": 1949},
+    {"titre": "Le Petit Prince", "auteur": "Antoine deSaint-Exupéry", "année": 1943},
+    {"titre": "Harry Potter", "auteur": "J.K. Rowling", "année": 1997},
+]
+
+aime_livres = [(1, "1984"), (1, "Le Petit Prince"), (3, "Harry Potter"), (4, "1984")]
